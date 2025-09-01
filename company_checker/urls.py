366ce606d9907fe39f7deaf6080cd5df9d1d619c
Jaspreet Singh <165907259+jassi-coder@users.noreply.github.com>
2025-09-01 15:11:27 +0530

    Update urls.py

--- company_checker/urls.py
+++ company_checker/urls.py
@@ -1,13 +1,8 @@
 # company_checker/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Welcome to Company Checker 🚀")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home,name="home"),
     path('',include('core.urls')
 ]
